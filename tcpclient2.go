9e80c90d04c510a0f310e41527d0b459207fd136
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-23 23:49:51 +0530

    examples

--- tcpclient2.go
+++ tcpclient2.go
@@ -33,3 +33,21 @@ func main() {
 		fmt.Println("failed to connect to server:", err)
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	// send text to server
+	_, err = conn.Write([]byte(text))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// read response
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("failed reading response:", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(buf[:n]))
+}
