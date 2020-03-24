4ef7d6313d1d9edda6ca2c48f694c3fa93b46370
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-24 23:43:59 +0530

    examples

--- udpc3.go
+++ udpc3.go
@@ -25,3 +25,29 @@ func main() {
 	// req is initialized with 0x1B or 0001 1011 which is
 	// a request setting for time server.
 	req[0] = 0x1B
+
+	// rsp byte slice used to receive server response
+	rsp := make([]byte, 48)
+
+	// setup generic connection (net.Conn) using net.Dial
+	conn, err := net.Dial("udp", host)
+	if err != nil {
+		fmt.Printf("failed to connect: %v\n", err)
+		os.Exit(1)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			fmt.Println("failed while closing connection:", err)
+		}
+	}()
+
+	fmt.Printf("requesting time from %s (%s)\n", host, conn.RemoteAddr())
+
+	// Once connection is established, the code pattern
+	// is the same as in the other impl.
+
+	// send time request
+	if _, err = conn.Write(req); err != nil {
+		fmt.Printf("failed to send request: %v\n", err)
+		os.Exit(1)
+	}
