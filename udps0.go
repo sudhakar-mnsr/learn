dfa0ffde669bb19f919699f9af31691427f6e47b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-24 23:49:51 +0530

    examples

--- udps0.go
+++ udps0.go
@@ -29,3 +29,26 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	// setup connection UDPConn with ListenUDP
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("failed to create socket:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Printf("listening for time requests: (udp) %s\n", conn.LocalAddr())
+
+	// From this point, the remainder of the code simply
+	// reads the incoming request and send a reponse.
+
+	// read incoming requests.
+	// since we are using a sessionless proto, each request can
+	// potentially go to a different client.  Therefore, the ReadFromXXX
+	// operation returns the remote address (saved in raddr)
+	// where to send the response.
+	_, raddr, err := conn.ReadFromUDP(make([]byte, 48))
+	if err != nil {
+		fmt.Println("error getting request:", err)
+		os.Exit(1)
+	}
