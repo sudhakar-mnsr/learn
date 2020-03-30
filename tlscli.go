c6b9cd863152eb5191b7858915722db4e07f2c15
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-30 20:43:19 +0530

    examples

--- tlscli.go
+++ tlscli.go
@@ -74,3 +74,23 @@ func main() {
 				continue
 			}
 		}
+		// Receive response
+		var currencies []curr.Currency
+		err = json.NewDecoder(conn).Decode(&currencies)
+		if err != nil {
+			switch err := err.(type) {
+			case net.Error:
+				fmt.Println("failed to receive response:", err)
+				continue
+			default:
+				fmt.Println("failed to decode response:", err)
+				continue
+			}
+		}
+
+		// print currencies
+		for i, c := range currencies {
+			fmt.Printf("%2d. %s[%s]\t%s, %s\n", i, c.Code, c.Number, c.Name, c.Country)
+		}
+	}
+}
