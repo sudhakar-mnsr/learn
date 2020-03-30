c5fc292c79238fccd4e4075557e94dc2af31d121
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-30 20:41:22 +0530

    examples

--- tlscli.go
+++ tlscli.go
@@ -30,3 +30,17 @@ const prompt = "currency"
 //  - n network protocol name [tcp,unix], default tcp
 //
 // Once started a prompt is provided to interact with service.
+
+func main() {
+	// setup flags
+	var addr, network, ca string
+	flag.StringVar(&addr, "e", "localhost:4443", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.StringVar(&ca, "ca", "../certs/ca-cert.pem", "CA certificate")
+	flag.Parse()
+
+	// Load our CA certificate
+	caCert, err := ioutil.ReadFile(ca)
+	if err != nil {
+		log.Fatal("failed to read CA cert", err)
+	}
