29cf8ccaff976643f0ebdbdea1cc05be2665524f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-31 20:27:35 +0530

    examples

--- tlscli2.go
+++ tlscli2.go
@@ -30,3 +30,13 @@ const prompt = "currency"
 //  - n network protocol name [tcp,unix], default tcp
 //
 // Once started a prompt is provided to interact with service.
+
+func main() {
+	// setup flags
+	var addr, network, cert, key, ca string
+	flag.StringVar(&addr, "e", "localhost:4443", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.StringVar(&cert, "cert", "../certs/client-cert.pem", "public cert")
+	flag.StringVar(&key, "key", "../certs/client-key.pem", "private key")
+	flag.StringVar(&ca, "ca", "../certs/ca-cert.pem", "root CA certificate")
+	flag.Parse()
