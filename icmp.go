4f5da9f4c27f7dc82152062db3e5ca8ba9f07511
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-02 21:54:25 +0530

    examples

--- icmp.go
+++ icmp.go
@@ -64,3 +64,10 @@ func main() {
         // receive a reply
         size, err2 := conn.Read(msg[0:])
         checkError(err2)
+        fmt.Print("Message received:")
+        for n := ipv4HeaderSize; n < size; n++ {
+                fmt.Print(" ", msg[n])
+        }
+        fmt.Println()
+        os.Exit(0)                                                  
+}
