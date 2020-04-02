47e56f533479f751d1de636a075545c775760b16
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-02 22:02:44 +0530

    examples

--- ftpserver.go
+++ ftpserver.go
@@ -13,3 +13,12 @@ const (
         CD  = "CD"
         PWD = "PWD"
 )
+
+func main() {
+
+        service := "0.0.0.0:1202"
+        tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+        checkError(err)
+
+        listener, err := net.ListenTCP("tcp", tcpAddr)
+        checkError(err)
