1564c2cfcbd2249d42da63e83e3e6938f3575f48
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-03 23:25:19 +0530

    examples

---
+++ httpc4.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://tools.ietf.org/rfc/rfc7540.txt", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Accept", "text/plain")
+	req.Header.Add("User-Agent", "SampleClient/1.0")
