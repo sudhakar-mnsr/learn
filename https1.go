e27c8cf6333e6c45dd723649831a9d89e4ffc807
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-03 23:28:12 +0530

    examples

---
+++ https1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "net/http"
+
+// msg implements handler
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/html")
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprint(resp, m)
+}
