db11c9990b02c39166136bcf3be3150d933bb3f8
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-03 23:34:05 +0530

    examples

--- http5.go
+++ http5.go
@@ -11,3 +11,9 @@ func main() {
 		resp.WriteHeader(http.StatusOK)
 		fmt.Fprint(resp, "Hello from Above!")
 	}
+
+	goodbye := func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Add("Content-Type", "text/html")
+		resp.WriteHeader(http.StatusOK)
+		fmt.Fprint(resp, "Goodbye, it's been real!")
+	}
