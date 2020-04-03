653f383b80cda15c1d1a38b1458558d1db883c4f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-03 23:30:40 +0530

    examples

---
+++ https4.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+import "net/http"
+
+// simple handler, no multiplex
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/html")
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprint(resp, m)
+}
