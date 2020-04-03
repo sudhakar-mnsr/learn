9fcccd31448c55140972558ed3a7c91df27ea26f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-03 23:31:05 +0530

    examples

--- https4.go
+++ https4.go
@@ -12,3 +12,10 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	resp.WriteHeader(http.StatusOK)
 	fmt.Fprint(resp, m)
 }
+
+
+// using default server
+func main() {
+	msgHandler := msg("Hello from high above!")
+	http.ListenAndServe(":4040", msgHandler)
+}
