84f58312445ff9ec8928c8ced3c03139b2f6f452
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-04 12:46:15 +0530

    examples

--- vladimirviven/learning-go/net/dial0.go
+++ vladimirviven/learning-go/net/dial0.go
@@ -12,6 +12,14 @@ func main() {
    addr := net.JoinHostPort(host, port)
    httpRequest := "GET /cache/epub/16328/pg16328.txt HTTP/1.1\n" + "Host: " + host + "\n\n"
    
+   // conn, err := net.Dial("udp", addr)
+   // The above statement is hanging. When experimenting remember to
+   // comment the tcp Dial line below. 
+   // Guess: I think this http server at gutenberg could not get
+   // port to return as UDP is connection less. Need more clarity
+   // on what exactly happens. Then add that detail and remove this 
+   // comment.
+   // #MNSRremove #MNSRclarity 
    conn, err := net.Dial("tcp", addr)
    if err != nil {
       fmt.Println(err)
