3b5f4fcb672bea15d4a772f1333fb22d158a2fe6
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-04 18:20:01 +0530

    examples

--- vladimirviven/learning-go/net/currency0/tcpserv0.go
+++ vladimirviven/learning-go/net/currency0/tcpserv0.go
@@ -33,3 +33,43 @@ func main() {
    }
 }
 
+func handleConn(conn net.Conn) {
+   defer conn.Close()
+   for {
+      cmdLine := make([]byte, 1024 * 4)
+      n, err := conn.Read(cmdLine)
+      if n == 0 || err != nil {
+         return
+      }
+      cmd, param := parseCommand(string(cmdLine[0:n]))
+      if cmd == "" {
+         continue
+      }
+   
+      // execute command
+      switch strings.ToUpper(cmd)
+      case "GET":
+         result := curr.Find(currencies, param)
+         if len(result) == 0 {
+            conn.Write([]byte("Nothing found\n"))
+            continue
+         }
+         // stream result to client
+         for _, cur := range result {
+            _, err := fmt.Fprintf(conn, "%s %s %s %s\n", cur.Name, cur.Code, cur.Number, cur.Country,)
+            if err != nil {
+               return
+            }
+         
+            // reset deadline while writing,
+            // causes server to close conn if client is gone
+            conn.SetWriteDeadline(time.Now().Add(time.second * 5)
+         }
+         // reset read deadline for next read
+         conn.SetWriteDeadline(time.Time{})
+      default:
+         conn.Write([]byte("Invalid command\n"))
+      }
+   }
+}
+      
