355ead82ae43d7b631bb8830a9ff32be2068abe2
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-05 23:34:55 +0530

    examples

--- vladimirviven/learning-go/net/jsonserv0.go
+++ vladimirviven/learning-go/net/jsonserv0.go
@@ -31,3 +31,11 @@ func currs(resp http.ResponseWriter, req *http.Request) {
       return
    }
 }
+
+func main() {
+   mux := http.NewServeMux()
+   mux.HandleFunc("/currency", currs)
+   if err := http.ListenAndServe(":4040", mux); err != nil {
+      fmt.Println(err)
+   }
+}
