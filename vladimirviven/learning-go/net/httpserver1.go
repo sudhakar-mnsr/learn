70ea082c0c2915c5e0ce30e8a26bba56ed8de7da
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-05 19:52:12 +0530

    examples

--- vladimirviven/learning-go/net/httpserver1.go
+++ vladimirviven/learning-go/net/httpserver1.go
@@ -3,7 +3,7 @@ package main
 import (
    "fmt"
    "net/http"
-   "time"
+//   "time"
 )
 
 type msg string
@@ -16,11 +16,14 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 func main() {
    msgHandler := msg("Hello from MNSR")
-   server := http.Server{
-           Addr: ":4040",
-           Handler: msgHandler,
-           ReadTimeout: time.Second * 5,
-           WriteTimeout: time.Second * 3,
-   }
-   server.ListenAndServe()
+   // server := http.Server{
+   //        Addr: ":4040",
+   //        Handler: msgHandler,
+   //        ReadTimeout: time.Second * 5,
+   //        WriteTimeout: time.Second * 3,
+   // }
+   // server.ListenAndServe()
+   // When configuration is not needed HTTP package includes a default 
+   // httpserver which is now used. This is enough for simple cases
+   http.ListenAndServe(":4040", msgHandler)
 }
