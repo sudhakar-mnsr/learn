ef6a6c67d933bec4a6b52816001005adb40a5561
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-05 19:17:00 +0530

    examples

---
+++ vladimirviven/learning-go/net/httpserver0.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "net/http"
+
+// msg implements handler
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+   resp.Header().Add("Content-Type", "text/html")
+   resp.WriteHeader(http.StatusOK)
+   fmt.Fprint(resp, m)
+}
+
+func main() {
+   msgHandler := msg("Hello from MNSR")
+   server := http.Server{Addr: ":4040", Handler: msgHandler}
+   server.ListenAndServe()
+}
