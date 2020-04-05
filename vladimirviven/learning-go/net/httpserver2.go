8743592f13a3a453af1f110d91324042c4b60c2b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-05 20:30:56 +0530

    examples

--- vladimirviven/learning-go/net/httpserver2.go
+++ vladimirviven/learning-go/net/httpserver2.go
@@ -6,6 +6,10 @@ import "net/http"
 
 func main() {
    mux := http.NewServeMux()
+   // It is worth noting that HTTP package makes default ServeMux 
+   // It is not necessary to use ServeMux. Use http.HandleFunc
+   // to map a path to a handler function as commented (see end).
+
    hello := func(resp http.ResponseWriter, req *http.Request) {
                resp.Header().Add("Content-Type", "text/html")
                resp.WriteHeader(http.StatusOK)
@@ -19,6 +23,10 @@ func main() {
    }
    mux.HandleFunc("/hello", hello)
    mux.HandleFunc("/goodbye", goodbye)
+   // http.HandleFunc("/hello", hello)
+   // http.HandleFunc("/goodbye", goodbye)
    
    http.ListenAndServe(":4040", mux)
+   // When you are using default Mux the start function is as below
+   // http.ListenAndServe(":4040", nil)
 }
