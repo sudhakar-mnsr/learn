53f8316f130ce4dc9c7ac4453d545ef7cffc5d08
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-06 16:00:27 +0530

    examples

---
+++ vladimirviven/learning-go/net/lsdns.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+   "context"
+   "flag"
+   "fmt"
+   "net"
+   "os"
+)
+
+var (
+   ip string
+   host string
+   ns bool
+   mx bool
+   txt bool
+   cname bool
+)
+
+func init() {
+   flag.StringVar(&ip, "ip", "", "IP address for DNS operation")
+   flag.StringVar(&host, "host", "", "Host address for DNS operation")
+   flag.BoolVar(&ns, "ns", false,"Host name server lookup")
+   flag.BoolVar(&mx, "mx", false,"Host domain mail server lookup")
+   flag.BoolVar(&txt, "txt", false,"Host domain TXT lookup")
+   flag.BoolVar(&cname, "cname", false,"Host CNAME lookup")
+}
+
+type lsdns struct {
+   resolver *net.Resolver
+}
+
+func newLsdns() *lsdns {
+   return &lsdns{net.DefaultResolver}
+}
+
+
