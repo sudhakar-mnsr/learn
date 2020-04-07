e16377f3d4778c4c92b74e93941dd85048e16fd0
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-07 23:21:21 +0530

    examples

--- vladimirviven/networking-go/tcpc1.go
+++ vladimirviven/networking-go/tcpc1.go
@@ -12,3 +12,24 @@ import (
 // the response on the screen.
 //
 // Usage: echoc -e <host-addr-endpoint> <text content>
+func main() {
+var addr string
+flag.StringVar(&addr, "e", "/tmp/echo2.sock", "service address endpoint")
+flag.Parse()
+text := flag.Arg(0)
+
+// use ResolveUnixAddr to create remote address to server
+raddr, err := net.ResolveUnixAddr("unix", addr)
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
+
+// Use DialUnix to create a connection to the remote address.
+// Note: there is no requirement to specify the local address.
+conn, err := net.DialUnix("unix, nil, raddr)
+if err != nil {
+   fmt.Println("failed to connect to server:", err)
+   os.Exit(1)
+}
+defer.Close()  
