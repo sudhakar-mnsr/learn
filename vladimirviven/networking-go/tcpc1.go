c4e0d5873cb4525c6129e42d1d26d0be088433f3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-07 23:22:04 +0530

    examples

--- vladimirviven/networking-go/tcpc1.go
+++ vladimirviven/networking-go/tcpc1.go
@@ -33,3 +33,20 @@ if err != nil {
    os.Exit(1)
 }
 defer.Close()  
+
+// send text to server
+_, err = conn.Write([]byte(text))
+if err != nil {
+	fmt.Println(err)
+	os.Exit(1)
+}
+
+// read response
+buf := make([]byte, 1024)
+n, err := conn.Read(buf)
+if err != nil {
+	fmt.Println("failed reading response:", err)
+	os.Exit(1)
+}
+fmt.Println(string(buf[:n]))
+}
