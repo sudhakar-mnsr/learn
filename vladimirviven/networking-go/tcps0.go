9f5a510329786e83c3e0e197215db41c4698be85
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-08 20:29:51 +0530

    examples

--- vladimirviven/networking-go/tcps0.go
+++ vladimirviven/networking-go/tcps0.go
@@ -14,27 +14,39 @@ import (
 // Usage:
 // echos -e <host:address>
 func main() {
-var addr string
-flag.StringVar(&addr, "e", ":4040", "service address endpoint")
-flag.Parse()
-
-// create local addr for socket
-laddr, err := net.ResolveTCPAddr("tcp", addr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
+   var addr string
+   flag.StringVar(&addr, "e", ":4040", "service address endpoint")
+   flag.Parse()
+   
+   // create local addr for socket
+   laddr, err := net.ResolveTCPAddr("tcp", addr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // announce service using ListenTCP
+   // which a TCPListener
+   l, err := net.ListenTCP("tcp", laddr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer l.Close()
+   fmt.Println("listening at (tcp), laddr.String())
+   
+   // req/response loop
+   for {
+      // use TCPListener to block and wait for TCP
+      // connection request using AcceptTCP which creates a TCPConn
+      conn, err := l.AcceptTCP()
+      if err != nil {
+         fmt.Println("failed to accept conn:", err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("connected to: ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
 }
-
-// announce service using ListenTCP
-// which a TCPListener
-l, err := net.ListenTCP("tcp", laddr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
-}
-defer l.Close()
-fmt.Println("listening at (tcp), laddr.String())
-
-
-
 
