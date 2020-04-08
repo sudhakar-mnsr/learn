cb10a348268fbfc009a6fba62585288eef2f34ad
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-08 20:33:48 +0530

    examples

---
+++ vladimirviven/networking-go/tcpc0.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+// This program implements a simple echo client over tcp.
+// It sends a text content to the server and displays
+// the response on the screen.
+//
+// Usage: echoc -e <host-addr-endpoint> <text content>
+func main() {
+var addr string
+flag.StringVar(&addr, "e", "localhost:4040", "service address endpoint")
+flag.Parse()
+text := flag.Arg(0)
+
+// use ResolveTCPAddr to create address to connect to
+raddr, err := net.ResolveTCPAddr("tcp", addr)
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
