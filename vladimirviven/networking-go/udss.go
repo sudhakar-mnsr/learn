5561409294c9329d86a87d24d16dc25e4ea2e6f1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-08 16:19:49 +0530

    examples

---
+++ vladimirviven/networking-go/udss.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+// This program is a simple Network Time Protocol server over
+// Unix Domain Socket instead of UDP. The implementation uses
+// ListenUnixgram and UnixConn to manage requests.
+// The server returns the number of seconds since 1900 up to the
+// current time.
+
+// Usage:
+// ntps -e <host address endpoint>
+
+func main() {
+   var path string
+   flag.StringVar("&path","e","/tmp/time.sock", "NTP server socket endpoint")
+   flag.Parse()
+   
+   // Create Unix address
+   addr, err := net.ResolveUnixAddr("unixgram", path)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // setup connection UnixConn with ListenUnixgram
+   conn, err := net.ListenUnixgram("unixgram", addr)
+   if err != nil {
+      fmt.Println("failed to create socket:", err)
+      os.Exit(1)
+   }
+   defer conn.Close()
+   fmt.Printf("listening on (unixgram) %s\n", conn.LocalAddr())
+   
+   for {
+      _, raddr, err := conn.ReadFromUnix(make([]byte,48))
+      if err != nil {
+         fmt.Println("error getting request: ", err)
+         os.Exit(1)
+      }
+      
+      if raddr == nil {
+         fmt.Println("warning: request missing remote addr")
+         continue
+      }
+      
+      go handleRequest(conn, raddr)
+   }
+}
