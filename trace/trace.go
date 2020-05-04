e0a108b0bdcee5c044e44a6968f7e0e33ee516ed
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-04 20:49:52 +0530

    examples

--- trace/trace.go
+++ trace/trace.go
@@ -93,3 +93,56 @@ func freq(topic string, docs []string) int {
    }
    return found
 }
+
+func freqConcurrent(topic string, docs []string) int {
+	var found int32
+
+	g := len(docs)
+	var wg sync.WaitGroup
+	wg.Add(g)
+
+	for _, doc := range docs {
+		go func(doc string) {
+			var lFound int32
+			defer func() {
+				atomic.AddInt32(&found, lFound)
+				wg.Done()
+			}()
+
+			file := fmt.Sprintf("%s.xml", doc[:8])
+			f, err := os.OpenFile(file, os.O_RDONLY, 0)
+			if err != nil {
+				log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
+				return
+			}
+
+			data, err := ioutil.ReadAll(f)
+			if err != nil {
+				f.Close()
+				log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
+				return
+			}
+			f.Close()
+
+			var d document
+			if err := xml.Unmarshal(data, &d); err != nil {
+				log.Printf("Decoding Document [%s] : ERROR : %v", doc, err)
+				return
+			}
+
+			for _, item := range d.Channel.Items {
+				if strings.Contains(item.Title, topic) {
+					lFound++
+					continue
+				}
+
+				if strings.Contains(item.Description, topic) {
+					lFound++
+				}
+			}
+		}(doc)
+	}
+
+	wg.Wait()
+	return int(found)
+}
