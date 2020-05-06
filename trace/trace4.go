f400b9b318a1a2d1fb5d9ac1b11c12c1ec47b0b5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-06 21:12:08 +0530

    examples

---
+++ trace/trace4.go
@@ -0,0 +1,34 @@
+// Sample program that performs a series of I/O related tasks to
+// better understand tracing in Go.
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"runtime/trace"
+	"strings"
+	"sync"
+	"sync/atomic"
+)
+
+type (
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+	}
+
+	channel struct {
+		XMLName xml.Name `xml:"channel"`
+		Items   []item   `xml:"item"`
+	}
+
+	document struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
+)
+
