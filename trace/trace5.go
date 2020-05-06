bd1acf3df4f7fa9ae21543a6f970ef99b78bc918
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-06 21:23:43 +0530

    examples

--- trace/trace5.go
+++ trace/trace5.go
@@ -71,3 +71,17 @@ func freqProcessors(topic string, docs []string) int {
 					log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
 					return
 				}
+
+				data, err := ioutil.ReadAll(f)
+				if err != nil {
+					f.Close()
+					log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
+					return
+				}
+				f.Close()
+
+				var d document
+				if err := xml.Unmarshal(data, &d); err != nil {
+					log.Printf("Decoding Document [%s] : ERROR : %v", doc, err)
+					return
+				}
