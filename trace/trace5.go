fd352eeeb15bcc743bd4801aeaee6201be54b458
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-06 21:22:06 +0530

    examples

--- trace/trace5.go
+++ trace/trace5.go
@@ -41,3 +41,8 @@ func main() {
 	for i := range docs {
 		docs[i] = fmt.Sprintf("newsfeed-%.4d.xml", i)
 	}
+	topic := "president"
+	n := freq(topic, docs)
+
+	log.Printf("Searching %d files, found %s %d times.", len(docs), topic, n)
+}
