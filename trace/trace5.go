e70aa18385ca70bbe3862d68c3d812262872116b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-06 21:19:31 +0530

    examples

--- trace/trace5.go
+++ trace/trace5.go
@@ -32,3 +32,12 @@ type (
 	}
 )
 
+func main() {
+
+        trace.Start(os.Stdout)
+	defer trace.Stop()
+
+	docs := make([]string, 4000)
+	for i := range docs {
+		docs[i] = fmt.Sprintf("newsfeed-%.4d.xml", i)
+	}
