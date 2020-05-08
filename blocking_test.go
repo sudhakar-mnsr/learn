abbc35fb37d8799ab5411ac1a5526e32f75a3175
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-08 23:15:07 +0530

    examples

--- blocking_test.go
+++ blocking_test.go
@@ -116,3 +116,12 @@ func stream(bufSize int) time.Duration {
 
 	// Send all the data to the receiving goroutine.
 	send(data, ch)
+
+	// Close the channel and wait for the receiving
+	// goroutine to terminate.
+	close(ch)
+	wg.Wait()
+
+	// Calculate how long the send took.
+	return time.Since(st)
+}
