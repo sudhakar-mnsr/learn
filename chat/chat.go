7c0fb2205edd3009622bd94b35aff214750bc027
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-09 20:21:48 +0530

    examples

--- chat/chat.go
+++ chat/chat.go
@@ -9,3 +9,15 @@ import (
 	"net"
 	"sync"
 )
+
+// temporary is declared to test for the existence of the method coming
+// from the net package.
+type temporary interface {
+	Temporary() bool
+}
+
+// message is the data received and sent to users in the chatroom.
+type message struct {
+	data string
+	conn net.Conn
+}
