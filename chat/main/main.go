eeb7382026ed979333739221673b27efbfcac457
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-10 15:53:02 +0530

    examples

---
+++ chat/main/main.go
@@ -0,0 +1,11 @@
+// This sample program demonstrates how to create a simple chat system.
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/ardanlabs/gotraining/topics/go/concurrency/patterns/chat"
+)
+
