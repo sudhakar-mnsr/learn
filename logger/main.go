3469e49201e1b0344a3de4e9446dabea0fcde107
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-11 00:40:48 +0530

    examples

--- logger/main.go
+++ logger/main.go
@@ -15,3 +15,11 @@ type device struct {
 	mu      sync.RWMutex
 	problem bool
 }
+
+// Write implements the io.Writer interface.
+func (d *device) Write(p []byte) (n int, err error) {
+
+	// Simulate disk problems.
+	for d.isProblem() {
+		time.Sleep(time.Second)
+	}
