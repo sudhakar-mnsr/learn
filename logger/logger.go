c51f99b08ac85565a7390a2bdeec801ea9964550
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-13 22:39:04 +0530

    examples

--- logger/logger.go
+++ logger/logger.go
@@ -14,3 +14,13 @@ type Logger struct {
 	write chan string    // Channel to send/recv data to be logged.
 	wg    sync.WaitGroup // Helps control the shutdown.
 }
+
+// New creates a logger value and initializes it for use. The user can
+// pass the size of the buffer to use for continuity.
+func New(w io.Writer, capacity int) *Logger {
+
+	// Create a value of type logger and init the channel
+	// and timer value.
+	l := Logger{
+		write: make(chan string, capacity), // Buffered channel if size > 0.
+	}
