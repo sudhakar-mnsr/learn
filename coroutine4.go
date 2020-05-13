63497cecb1b14b93c7ce237b8d4cb64b0b305907
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-14 04:23:06 +0530

    examples

---
+++ coroutine4.go
@@ -0,0 +1,13 @@
+// Create a program that declares two anonymous functions. One that counts down from
+// 100 to 0 and one that counts up from 0 to 100. Display each number with an
+// unique identifier for each goroutine. Then create goroutines from these functions
+// and don't let main return until the goroutines complete.
+//
+// Run the program in parallel.
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
