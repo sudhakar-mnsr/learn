37e6d6018282b7cc12771a587b3452792ed1b58a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-18 05:55:12 +0530

    examples

---
+++ list/list_test.go
@@ -0,0 +1,10 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package list
+	// Node represents the data being stored.
+	type Node struct {
+		Data string
+		next *Node
+		prev *Node
+	}
