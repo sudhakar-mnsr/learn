1fcdabe8293f1a7150ba27b315acad1d0c40d451
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-20 20:56:58 +0530

    examples

---
+++ strings/palindrome_test.go
@@ -0,0 +1,7 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package palindrome
+	// Is checks if a string is a Palindrome.
+	func Is(input string) bool
+*/
