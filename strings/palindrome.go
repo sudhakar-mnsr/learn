ee9d988e57c0390831ed75a8541fadd6edfe3aef
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-20 20:55:33 +0530

    examples

--- strings/palindrome.go
+++ strings/palindrome.go
@@ -3,3 +3,9 @@ package palindrome
 import "github.com/ardanlabs/gotraining/topics/go/algorithms/strings/reverse"
 
 // Is checks if a string is a Palindrome.
+func Is(input string) bool {
+
+	// If the input string is empty or as a length of 1 return true.
+	if input == "" || len(input) == 1 {
+		return true
+	}
