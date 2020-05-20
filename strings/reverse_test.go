cb65a0c78f56791c196e696fa6236e93411efe9b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-20 21:02:30 +0530

    examples

---
+++ strings/reverse_test.go
@@ -0,0 +1,7 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package reverse
+	// String takes the specified string and reverses it.
+	func String(str string) string
+*/
