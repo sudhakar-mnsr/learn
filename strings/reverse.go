463e20ba272b1b9eb88592eb7eb81a10ea85d30b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-20 21:01:46 +0530

    examples

--- strings/reverse.go
+++ strings/reverse.go
@@ -5,6 +5,7 @@ func String(str string) string {
 
 	// Convert the input string into slice of runes for processing.
 	// A rune represent a code point in the UTF-8 character set.
+
 	runes := []rune(str)
 	// Create an index that will traverse the collection of
 	// runes from the beginning to the end.
@@ -13,3 +14,11 @@ func String(str string) string {
 	// Create an index that will traverse the collection of
 	// runes from the end to the beginning.
 	end := len(runes) - 1
+
+	// Keep swapping runes until the two indexes meet in the middle.
+	for beg < end {
+
+		// Swap the position of these two rune’s.
+		r := runes[beg]
+		runes[beg] = runes[end]
+		runes[end] = r
