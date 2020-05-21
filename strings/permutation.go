dad8b8af7eb942b533eeccc875a18919b06f2e7d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-21 23:04:11 +0530

    examples

--- strings/permutation.go
+++ strings/permutation.go
@@ -6,3 +6,8 @@ import (
 
 // RuneSlice a custom type of a slice of runes.
 type RuneSlice []rune
+
+// For sorting an RuneSlice.
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
