a738d3d567c29c2bd50da99082636168aa697dc1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-21 23:05:43 +0530

    examples

---
+++ strings/permutation_test.go
@@ -0,0 +1,7 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package permutation
+	// Is check if two strings are permutations.
+	func Is(str1, str2 string) bool
+*/
