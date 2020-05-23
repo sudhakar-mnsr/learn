983ae4207125bb98f95fb14c857e8808abe8bf13
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-24 01:23:48 +0530

    examples

--- queue_test.go
+++ queue_test.go
@@ -225,3 +225,8 @@ func TestDequeueEmpty(t *testing.T) {
 		const items = 5
 		t.Logf("\tTest 0:\tWhen enqueuing %d items", items)
 		{
+			q, err := queue.New(items)
+			if err != nil {
+				t.Fatalf("\t%s\tTest 0:\tShould be able to create a queue for %d items : %v", failed, items, err)
+			}
+			t.Logf("\t%s\tTest 0:\tShould be able to create a queue for %d items.", succeed, items)
