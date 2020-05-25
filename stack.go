7b21990469a4cfe9d729485ac4bad6014f0d4578
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-25 22:52:57 +0530

    examples

---
+++ stack.go
@@ -0,0 +1,9 @@
+// Package stack asks the student to implement a stack in Go.
+package stack
+
+import "errors"
+
+// Data represents what is being stored on the stack.
+type Data struct {
+	Name string
+}
