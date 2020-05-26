ba022d7599810a97cc68b34e81547fd0d07e4797
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-26 22:03:11 +0530

    examples

---
+++ stack_test.go
@@ -0,0 +1,8 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package stack
+	// Data represents what is being stored on the stack.
+	type Data struct {
+		Name string
+	}
