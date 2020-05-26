d82acdf459bff54cc7d574158f5cb356316d8ae7
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-26 22:03:33 +0530

    examples

--- stack_test.go
+++ stack_test.go
@@ -6,3 +6,12 @@
 	type Data struct {
 		Name string
 	}
+
+	// Stack represents a stack of data.
+	type Stack struct {
+		data []*Data
+	}
+	// Make allows the creation of a stack with an initial
+	// capacity for efficiency. Otherwise a stack can be
+	// used in its zero value state.
+	func Make(cap int) *Stack
