31023ce65664f08015d7b4be8b6326034dae7a51
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-28 22:29:01 +0530

    examples

--- max.go
+++ max.go
@@ -4,3 +4,7 @@ import "fmt"
 
 // Max returns the maximum integer in the slice.
 func Max(n []int) (int, error) {
+	// First check there are numbers in the collection.
+	if len(n) == 0 {
+		return 0, fmt.Errorf("slice %#v has no elements", n)
+	}
