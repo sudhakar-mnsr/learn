8b6a68af790b5d6cbc96d6b05ce9c5accfd01d67
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-30 02:33:41 +0530

    examples

---
+++ min_test.go
@@ -0,0 +1,7 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package min
+	// Min returns the minimum integer in the slice.
+	func Min(n []int) (int, error)
+*/
