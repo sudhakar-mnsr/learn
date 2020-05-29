dc0ad6f691a62da93f839e482b99b51af826cf5d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-30 02:32:09 +0530

    examples

--- min.go
+++ min.go
@@ -4,3 +4,8 @@ import "fmt"
 
 // Min returns the minimum integer in the slice.
 func Min(n []int) (int, error) {
+
+	// First check there are numbers in the collection.
+	if len(n) == 0 {
+		return 0, fmt.Errorf("slice %#v has no elements", n)
+	}
