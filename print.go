08fc3c7e46de2e59a115387ef1add74f87b2d5b4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-02 20:32:02 +0530

    examples

--- print.go
+++ print.go
@@ -4,3 +4,11 @@ import (
 	"fmt"
 	"math"
 )
+
+// PrettyPrint takes a Tree value and displays a pretty print
+// version of the tree.
+func PrettyPrint(t *Tree) {
+
+	// Build an index map of positions for print layout.
+	values := make(map[int]int)
+	maxIdx := buildIndexMap(values, 0, 0, t.root)
