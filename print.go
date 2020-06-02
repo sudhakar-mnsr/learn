a0e5ef745f2a8777285669bea2da56553fb9f161
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-02 20:32:27 +0530

    examples

--- print.go
+++ print.go
@@ -12,3 +12,10 @@ func PrettyPrint(t *Tree) {
 	// Build an index map of positions for print layout.
 	values := make(map[int]int)
 	maxIdx := buildIndexMap(values, 0, 0, t.root)
+
+	// Calculate the total number of levels based on
+	// the max index provided.
+	var levels int
+	for {
+		pow := math.Pow(2, float64(levels))
+		if maxIdx < int(pow) {
