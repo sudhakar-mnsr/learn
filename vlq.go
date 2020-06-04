87293471ee6486f8574daa382cb66ae7d5691ec5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-04 22:37:45 +0530

    examples

--- vlq.go
+++ vlq.go
@@ -4,3 +4,11 @@ import (
 	"math"
 	"math/bits"
 )
+
+// DecodeVarint takes a variable length VLQ based integer and
+// decodes it into a 32 bit integer.
+func DecodeVarint(input []byte) (uint32, error) {
+	const lastBitSet = 0x80 // 1000 0000
+
+	var d uint32
+	var bitPos int
