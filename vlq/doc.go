815d0c217ad00664c9f949472f9f398c82545cd0
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-05 10:52:07 +0530

    examples

--- vlq/doc.go
+++ vlq/doc.go
@@ -12,3 +12,17 @@ So, if an integer is between 0-127, it can be represented as one byte. The
 largest integer allowed is 0FFFFFFF, which translates to 4 bytes variable
 length. Here are examples of delta-times as 32-bit values, and the variable
 length quantities that they translate to:
+
+ NUMBER        VARIABLE QUANTITY
+00000000              00
+00000040              40
+0000007F              7F
+00000080             81 00
+00002000             C0 00
+00003FFF             FF 7F
+00004000           81 80 00
+00100000           C0 80 00
+001FFFFF           FF FF 7F
+00200000          81 80 80 00
+08000000          C0 80 80 00
+0FFFFFFF          FF FF FF 7F
