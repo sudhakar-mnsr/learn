8c7474a4276cf9370dfb9da49fe14614fe3d0160
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-05 10:51:41 +0530

    examples

--- vlq/doc.go
+++ vlq/doc.go
@@ -7,3 +7,8 @@ you have to unpack it into a series of 7-bit bytes. Of course, you will have
 a variable number of bytes depending upon your integer. To indicate which
 is the last byte of the series, you leave bit #7 clear. In all of the
 preceding bytes, you set bit #7.
+
+So, if an integer is between 0-127, it can be represented as one byte. The
+largest integer allowed is 0FFFFFFF, which translates to 4 bytes variable
+length. Here are examples of delta-times as 32-bit values, and the variable
+length quantities that they translate to:
