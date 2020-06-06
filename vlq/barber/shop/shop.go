992ce2daa0685f6af8457dccbebaa04dc3a2cd00
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-06 23:59:42 +0530

    examples

--- vlq/barber/shop/shop.go
+++ vlq/barber/shop/shop.go
@@ -44,3 +44,13 @@ type Shop struct {
 	wgClose sync.WaitGroup // Provides support for closing the shop.
 	wgEnter sync.WaitGroup // Tracks customers entering the shop.
 }
+
+// Open creates a new shop for business and gets the barber working.
+func Open(maxChairs int) *Shop {
+	s := Shop{
+		chairs: make(chan customer, maxChairs),
+	}
+	atomic.StoreInt32(&s.open, 1)
+
+	// Get the barber working.
+	s.wgClose.Add(1)
