5ccf425af4064f436d32d2512b96c301c31f058a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-11 21:31:00 +0530

    examples

---
+++ example1.go
@@ -0,0 +1,7 @@
+// Using the template, declare a set of concrete types that implement the set
+// of predefined interface types. Then create values of these types and use
+// them to complete a set of predefined tasks.
+package main
+
+// Add import(s).
+import "fmt"
