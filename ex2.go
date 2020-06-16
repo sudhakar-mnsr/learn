368cf0e75b449f38ff217fcd2fe0283acd28ed0a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-16 22:52:05 +0530

    examples

---
+++ ex2.go
@@ -0,0 +1,13 @@
+// $ ./example2 | cut -c1 | grep '[AB]' | uniq
+
+// Sample program to show how the goroutine scheduler
+// will time slice goroutines on a single thread.
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
