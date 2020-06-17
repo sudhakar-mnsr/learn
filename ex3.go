146dffe0b408a0698672cb4e9941d56ad7213e7d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-17 10:22:44 +0530

    examples

--- ex3.go
+++ ex3.go
@@ -14,3 +14,10 @@ func init() {
 	// Allocate two logical processors for the scheduler to use.
 	runtime.GOMAXPROCS(2)
 }
+
+func main() {
+
+	// wg is used to wait for the program to finish.
+	// Add a count of two, one for each goroutine.
+	var wg sync.WaitGroup
+	wg.Add(2)
