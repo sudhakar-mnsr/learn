25e5b8d10049451c8d8dbe4ec9fce867854fc7a4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-19 23:09:43 +0530

    examples

---
+++ chan1.go
@@ -0,0 +1,10 @@
+// Write a program where two goroutines pass an integer back and forth
+// ten times. Display when each goroutine receives the integer. Increment
+// the integer with each pass. Once the integer equals ten, terminate
+// the program cleanly.
+package main
+
+import (
+	"fmt"
+	"sync"
+)
