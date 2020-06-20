8b544f998569012edba0788f067bafd7e0b5270a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-20 21:15:34 +0530

    examples

---
+++ chan2.go
@@ -0,0 +1,13 @@
+// Write a program that uses a fan out pattern to generate 100 random numbers
+// concurrently. Have each goroutine generate a single random number and return
+// that number to the main goroutine over a buffered channel. Set the size of
+// the buffer channel so no send every blocks. Don't allocate more buffers than
+// you need. Have the main goroutine display each random number is receives and
+// then terminate the program.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
