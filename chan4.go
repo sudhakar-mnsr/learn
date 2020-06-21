8646ca6c1f6ccaf10d3f21f96c585ed8a761a43d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-21 19:46:31 +0530

    examples

---
+++ chan4.go
@@ -0,0 +1,11 @@
+// Write a program that creates a fixed set of workers to generate random
+// numbers. Discard any number divisible by 2. Continue receiving until 100
+// numbers are received. Tell the workers to shut down before terminating.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+)
