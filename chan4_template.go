7bf81bdf71d8789382e20bb09e4acb2cf50d9628
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-22 21:46:39 +0530

    examples

--- chan4_template.go
+++ chan4_template.go
@@ -7,3 +7,11 @@ import (
 	"sync"
 	"time"
 )
+
+// numbers maintains a set of random numbers.
+var numbers []int
+
+// init is called prior to main.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
