e402bf697e66d782f715c51fc1ef07b365d4329e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-29 21:24:07 +0530

    examples

--- generic1.go
+++ generic1.go
@@ -15,3 +15,10 @@ func main() {
 	}
 	v1, err := retry(string)(context.Background(), time.Second, worker1)
 	fmt.Println(v1, err)
+
+	worker2 := func(ctx context.Context) (int, error) {
+		time.Sleep(time.Millisecond)
+		return 9999999, nil
+	}
+	v2, err := retry(int)(context.Background(), time.Second, worker2)
+	fmt.Println(v2, err)
