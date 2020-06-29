a73f797cf8d55f17387a57c22e6463454debf5ca
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-29 21:23:49 +0530

    examples

--- generic1.go
+++ generic1.go
@@ -7,3 +7,11 @@ import (
 	"fmt"
 	"time"
 )
+
+func main() {
+	worker1 := func(ctx context.Context) (string, error) {
+		time.Sleep(time.Millisecond)
+		return "test string", nil
+	}
+	v1, err := retry(string)(context.Background(), time.Second, worker1)
+	fmt.Println(v1, err)
