4095d66d5a32e366c47c91b4a9987f948c4720e1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-30 21:20:12 +0530

    examples

--- interface.go
+++ interface.go
@@ -6,3 +6,11 @@ import (
 	"fmt"
 	"time"
 )
+
+func main() {
+	worker1 := func(ctx context.Context) (interface{}, error) {
+		time.Sleep(time.Millisecond)
+		return "test string", nil
+	}
+	v1, err := retry(context.Background(), time.Second, worker1)
+	fmt.Println(v1.(string), err)
