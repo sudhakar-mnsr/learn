b3b85065fcb7354470531326a68a73b5d42c6cee
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-06-30 21:20:30 +0530

    examples

--- interface.go
+++ interface.go
@@ -14,3 +14,10 @@ func main() {
 	}
 	v1, err := retry(context.Background(), time.Second, worker1)
 	fmt.Println(v1.(string), err)
+
+	worker2 := func(ctx context.Context) (interface{}, error) {
+		time.Sleep(time.Millisecond)
+		return 9999999, nil
+	}
+	v2, err := retry(context.Background(), time.Second, worker2)
+	fmt.Println(v2.(int), err)
