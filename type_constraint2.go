276bfc9a1264f94799409d54f92f03cc5a9478bc
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-03 21:11:14 +0530

    examples

--- type_constraint2.go
+++ type_constraint2.go
@@ -6,3 +6,10 @@ func main() {
     fmt.Println(Add(10, 20))
     fmt.Println(Add("A", "B"))
     // fmt.Println(Add(3.14159, 2.96))
+
+    d := []duration{5000, 10, 40}
+    fmt.Println("Matched at index: ", match(d, 10))
+
+    // f := []fruit{"apple", "orange", "banna"}
+    // fmt.Println("Matched at index: ", match(f, "banna"))
+}
