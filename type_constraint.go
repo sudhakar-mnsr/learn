50253ac66f65eae297e5b9b9675d0168e697ff3f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-03 21:09:12 +0530

    examples

--- type_constraint.go
+++ type_constraint.go
@@ -19,3 +19,8 @@ type operateFunc(type V) func(v V) V
 type SliceConstraint(type T) interface {
 	type []T
 }
+
+// The interesting code here is how V is used to satisfy type T
+// for the SliceConstraint interface.
+func operate(type T SliceConstraint(V), V interface{})(slice T, fn operateFunc(V)) T {
+	ret := make(T, len(slice))
