80eada5ee6ffccb38bb39012437d2e23aebfd6ac
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-04 22:47:49 +0530

    examples

--- behavior_constraint/concrete.go
+++ behavior_constraint/concrete.go
@@ -4,3 +4,8 @@ import "fmt"
 
 func main() {
 	users := []user{
+		{name: "Bill", email: "bill@ardanlabs.com"},
+		{name: "Ale", email: "ale@whatever.com"},
+	}
+	s := stringifyUsers(users)
+	fmt.Println(s)
