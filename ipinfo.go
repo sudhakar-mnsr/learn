84a4b7b28c1c4ae76ae5b0739a19e0e3aa4d6762
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-26 22:53:02 +0530

    examples

--- ipinfo.go
+++ ipinfo.go
@@ -7,4 +7,15 @@ import (
 )
 
 func main() {
+if len(os.Args) != 2 {
+   fmt.Println("Missing ip address")
+   os.Exit(1)
+}
+
+ip = net.IP(os.Args[1])
+if ip == nil {
+   fmt.Println("Unable to parse IP address")
+   fmt.Println("Address should use IPv4 dot-notation or IPv6 colon-notation")
+   os.Exit(1)
+} 
 }
