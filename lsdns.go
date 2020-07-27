ca770f19ac749f3169097fa488a058b036a28adb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-27 20:32:55 +0530

    examples

---
+++ lsdns.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+   "context"
+   "fmt"
+   "os"
+   "net"
+   "flag"
+)
+
+var (
+   ip string
+   host string
+   ns bool
+   mx bool
+   txt bool
+   cname bool
+)
+
+func init() {
+   flag.StringVar(&ip, "ip", "", "IP address for DNS operation")
+   flag.StringVar(&host, "host", "", "Host name for DNS operation")
+   flag.BoolVar(&ns, "ns", false, "Host name server lookup")
+   flag.BoolVar(&mx, "mx", false, "Host domain mail server lookup")
+   flag.BoolVar(&txt, "txt", false, "Host domain TXT lookup")
+   flag.BoolVar(&cname, "cname", false, "Host CNAME lookup")
+}
