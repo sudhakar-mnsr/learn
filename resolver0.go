0a913c94697c33700d3fc0a2a4a632585f808b00
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-27 14:52:34 +0530

    examples

--- resolver0.go
+++ resolver0.go
@@ -12,10 +12,21 @@ var (
    host string
 )
 
+// This program looksup IP address associated with the hostname
+// This program uses net.Resolver directly to specify resolver
+// net.Resolver{PureGo:true}
+
 func main() {
-flag.StringVar(&host, "host", "localhost", "host name to resolve")
-flag.Parse()
-
-res := net.Resolver{PreferGo: true}
-addrs, err := res.LookupHost(context.Background(), host)
-
+   flag.StringVar(&host, "host", "localhost", "host name to resolve")
+   flag.Parse()
+   
+   res := net.Resolver{PreferGo: true}
+   addrs, err := res.LookupHost(context.Background(), host)
+   
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   fmt.Println(addrs)
+}
