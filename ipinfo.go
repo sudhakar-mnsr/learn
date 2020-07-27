991e6d1ed237e3643f7b4e743e09499838c44ddd
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-27 14:53:14 +0530

    examples

--- ipinfo.go
+++ ipinfo.go
@@ -7,28 +7,28 @@ import (
 )
 
 func main() {
-if len(os.Args) != 2 {
-   fmt.Println("Missing ip address")
-   os.Exit(1)
-}
-
-ip = net.IP(os.Args[1])
-if ip == nil {
-   fmt.Println("Unable to parse IP address")
-   fmt.Println("Address should use IPv4 dot-notation or IPv6 colon-notation")
-   os.Exit(1)
-} 
-
-fmt.Println()
-fmt.Println("IP:                     %s\n", ip)
-fmt.Println("Default Mask:           %s\n", net.IP(ip.DefaultMask()))
-fmt.Println("Loopback:               %t\n", ip.IsLoopback())
-fmt.Println("Unicast:")
-fmt.Println("   Global:              %t\n", ip.IsGlobalUnicast())
-fmt.Println("   Link:                %t\n", ip.IsLinkLocalUnicast())
-fmt.Println("Multicast:")
-fmt.Println("   Global:              %t\n", ip.IsMulticast())
-fmt.Println("   Interface:           %t\n", ip.IsInterfaceLocalMulticast())
-fmt.Println("   Link:                %t\n", ip.IsLinkLocalMulticast())
-fmt.Println()
+   if len(os.Args) != 2 {
+      fmt.Println("Missing ip address")
+      os.Exit(1)
+   }
+   
+   ip = net.IP(os.Args[1])
+   if ip == nil {
+      fmt.Println("Unable to parse IP address")
+      fmt.Println("Address should use IPv4 dot-notation or IPv6 colon-notation")
+      os.Exit(1)
+   } 
+   
+   fmt.Println()
+   fmt.Println("IP:                     %s\n", ip)
+   fmt.Println("Default Mask:           %s\n", net.IP(ip.DefaultMask()))
+   fmt.Println("Loopback:               %t\n", ip.IsLoopback())
+   fmt.Println("Unicast:")
+   fmt.Println("   Global:              %t\n", ip.IsGlobalUnicast())
+   fmt.Println("   Link:                %t\n", ip.IsLinkLocalUnicast())
+   fmt.Println("Multicast:")
+   fmt.Println("   Global:              %t\n", ip.IsMulticast())
+   fmt.Println("   Interface:           %t\n", ip.IsInterfaceLocalMulticast())
+   fmt.Println("   Link:                %t\n", ip.IsLinkLocalMulticast())
+   fmt.Println()
 }
