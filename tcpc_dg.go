0f2ae0140b173a08f2b1df66318d0a62c020399c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-30 20:50:02 +0530

    examples

--- tcpc_dg.go
+++ tcpc_dg.go
@@ -10,15 +10,38 @@ import (
 // Simple echo server over Unix domain socket
 
 func main() {
-var addr string
-flag.StringVar(&host, "e", "/tmp/tcpc.sock", "service address endpoint")
-flag.Parse()
-text := flag.Arg(0)
-
-// use ResolveUnixAddr to create remote address to server
-raddr, err := net.ResolveUnixAddr("unix", addr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
+   var addr string
+   flag.StringVar(&host, "e", "/tmp/tcpc.sock", "service address endpoint")
+   flag.Parse()
+   text := flag.Arg(0)
+   
+   // use ResolveUnixAddr to create remote address to server
+   raddr, err := net.ResolveUnixAddr("unix", addr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // use DialUnix to create a connection to remote address.
+   // Note: there is no requirement to specify local address
+   conn, err := net.DialUnix("unix", nil, raddr)
+   if err != nil {
+      fmt.Println("failed to connect to server:", err)
+      os.Exit(1)
+   }
+   defer conn.Close()
+   
+   _, err = conn.Write([]byte(text))
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   buf := make([]byte, 1024)
+   n, err := conn.Read(buf)
+   if err != nil {
+      fmt.Println("failed reading response:", err)
+      os.Exit(1)
+   }
+   fmt.Println(string(buf[:n]))
 }
-
