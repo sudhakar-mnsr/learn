a4067be6e6c3f63efd0e011bda8e520d2bb1e65d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-30 12:59:04 +0530

    examples

--- udps_generic.go
+++ udps_generic.go
@@ -22,4 +22,20 @@ func main() {
    flag.StringVar(&network, "n", "udp", "udp or unixgram")
    flag.Parse()
 
+   switch network {
+   case "udp", "udp4", "udp6", "unixgram":
+   default:
+      fmt.Println("unsupported network:", network)
+      os.Exit(1)
+   }
 
+   // create a generic packet connection, PacketConn, with ListenPacket.
+   // PacketConn implements common ReadFrom and WriteTo that are 
+   // protocol agnostic
+   conn, err := net.ListenPacket(network, host)
+   if err != nil {
+      fmt.Println("failed to create socket:", err)
+      os.Exit(1)
+   }
+   defer conn.Close()
+   fmt.Printf("listening on (%s)%s\n", network, conn.LocalAddr())
