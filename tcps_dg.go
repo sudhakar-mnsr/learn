932dc9ff0e5550fd91b2dee647080803e7f62650
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-30 20:58:34 +0530

    examples

--- tcps_dg.go
+++ tcps_dg.go
@@ -10,23 +10,36 @@ import (
 // Simple echo server over Unix Domain socket (streaming)
 
 func main() {
-var addr string
-flag.StringVar(&addr, "e", "/tmp/tcps.sock", "service endpoint address")
-flag.Parse()
-
-laddr, err := net.ResolveUnixAddr("unix", addr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
+   var addr string
+   flag.StringVar(&addr, "e", "/tmp/tcps.sock", "service endpoint address")
+   flag.Parse()
+   
+   laddr, err := net.ResolveUnixAddr("unix", addr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // announce service using LIstenUnix which creates UnixListener
+   l, err := net.ListenUnix("unix", laddr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer l.Close()
+   fmt.Println("listening at (unix)", laddr.String())
+   
+   for {
+      // use UnixListener to block and wait for UDS connection request
+      // using AcceptUnix which then creates UnixConn
+      conn, err := l.AcceptUnix()
+      if err != nil {
+         fmt.Println("failed to accept conn:", err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("connected to: ", conn.RemoteAddr())
+   
+      go handleConnection(conn)
+   }
 }
-
-// announce service using LIstenUnix which creates UnixListener
-l, err := net.ListenUnix("unix", laddr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
-}
-defer l.Close()
-fmt.Println("listening at (unix)", laddr.String())
-
-
