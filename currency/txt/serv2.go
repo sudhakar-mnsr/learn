17dca1339dc7a36984b02fdc8d5b518e36db7e54
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-01 02:21:44 +0530

    examples

--- currency/txt/serv2.go
+++ currency/txt/serv2.go
@@ -54,3 +54,73 @@ func main() {
       go handleConnection(conn)
    }
 }
+
+func handleConnection(conn net.Conn) {
+   defer func() {
+      if err := conn.Close(); err != nil {
+         log.Println("error closing connection:", err)
+      }
+   }()
+
+   if _, err := conn.Write([]byte("Connected...\nUsage: Get <currency, country, or code>\n")); err != nil {
+      log.Println("error writing:", err)
+      return
+   }
+
+   appendBytes := func(dest, src []byte) ([]byte, error) {
+      for _, b := range src {
+         if b == '\n' {
+            return dest, io.EOF
+         }
+         dest = append(dest, b)
+      }
+      return dest, nil
+   }
+   for {
+      var cmdLine []byte
+      for {
+         chunk := make([]byte, 4)
+         n, err := conn.Read(chunk)
+         if err != nil {
+            if err == io.EOF {
+               cmdLine, _ = appendBytes(cmdLine, chunk[:n])
+               break
+            }
+            log.Println("connection read error:", err)
+            return
+         }
+         if cmdLine, err = appendBytes(cmdLine, chunk[:n]); err == io.EOF {
+            break
+         }
+         cmd, param := parseCommand(string(cmdLine))
+         if cmd == "" {
+            if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+               log.Println("failed to write:", err)
+            }
+            continue
+         }
+
+         switch string.ToUpper(cmd) {
+         case "GET":
+            result := curr.Find(currencies, param)
+            if len(result) == 0 {
+               if _, err := conn.Write([]byte("Nothing found\n")); err != nil {
+                  log.Println("failed to write:"err)
+                }
+                continue
+            }
+            for _, cur := range result {
+               _, err := conn.Write([]byte(fmt.Sprintf(%s %s %s %s\n", cur.Name, cur.code, cur.Number, cur.Country,),))
+            if err != nil {
+               log.Println("failed to write response", err)
+               return
+            }
+         }
+      default:
+         if _, err  conn.Write([]byte("Invalid command\n)); err != nil {
+            log.Println("failed to wirte", err)
+            return
+         }
+      }
+   }
+}          
