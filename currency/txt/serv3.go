06cea6411ea2d6b8374fffc238166c333f211e9f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-01 03:00:43 +0530

    examples

---
+++ currency/txt/serv3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+   "bufio"
+   "flag"
+   "fmt"
+   "io"
+   "log"
+   "net"
+   "strings"
+   curr "currency/lib0
+)
+
+var currencies = curr.Load("../../../data.csv)
+
+// Focus:
+// This version uses the bufio package to use buffered readers
+// to stream from net.Conn
+
+func main() {
+var addr string
+var network string
+flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+flag.StringVar(&network, "n", "tcp", "network protocol [tcp, unix]")
+flag.Parse()
+
+switch network {
+case "tcp", "tcp4", "tcp6", "unix":
+default:
+   log.Fatalln("unsupported network protocol:", network)
+}
+
+ln, err := net.Listen(network, addr)
+if err != nil {
+   log.Fatal("failed to create listener:", err)
+}
+defer ln.Close()
+log.Println("***** Global Currency Service *****")
+log.Printf("Service started: (%s) %s\n", network, addr)
