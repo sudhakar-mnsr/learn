43bf65a8c263e8daa04074670b9c4e5984fb16b9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-07-31 23:57:59 +0530

    examples

--- currency/txt/serv2.go
+++ currency/txt/serv2.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-"flag"
-"fmt"
-"io"
-"log"
-"net"
-"strings"
-curr "currency/lib0
+   "flag"
+   "fmt"
+   "io"
+   "log"
+   "net"
+   "strings"
+   curr "currency/lib0
 )
+
+var currencies = curr.Load("../../../data.csv")
+
+// Focus:
+// This version of currency server focuses on implementing streaming
+// strategy when receiving data from client to avoid dropping data
+// when request is larger than internal buffer. This relies on the fact
+// that net.Conn implements io.Reader
+
+func main() {
+	var addr string
+	var network string
+	flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.Parse()
+
+	// validate supported network protocols
+	switch network {
+	case "tcp", "tcp4", "tcp6", "unix":
+	default:
+		log.Fatalln("unsupported network protocol:", network)
+	}
