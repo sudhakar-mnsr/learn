3ef28dad7367f0c0987ba68d339b284508204042
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-01 23:31:43 +0530

    examples

--- currency/txt1/serv1.go
+++ currency/txt1/serv1.go
@@ -19,25 +19,39 @@ var currencies = curr.Load("../../../data.csv")
 // Buffers are read in one shot creating opportunities for missing data
 
 func main() {
-var addr string
-var network string
-flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp", "tcp4", "tcp6", "unix":
-default:
-	log.Fatalln("unsupported network protocol:", network)
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
+   default:
+   	log.Fatalln("unsupported network protocol:", network)
+   }
+   
+   // create listener for provided network and host address
+   ln, err := net.Listen(network, addr)
+   if err != nil {
+      log.Fatal("failed to create listener:", err)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started: (%s) %s\n", network, addr)
+   
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         fmt.Println(err)
+         if err := conn.Close(); err != nil {
+            log.Println("failed to close listener:", err)
+         }
+         continue
+      }
+      log.Println("Connected to", conn.RemoteAddr())
+      
+      go handleConnection(conn)
+   }
 }
-
-// create listener for provided network and host address
-ln, err := net.Listen(network, addr)
-if err != nil {
-   log.Fatal("failed to create listener:", err)
-}
-defer ln.Close()
-log.Println("***** Global Currency Service *****")
-log.Printf("Service started: (%s) %s\n", network, addr)
-
