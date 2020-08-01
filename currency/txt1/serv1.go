76b5416b6d6b55f799d734aa07e74f34b9ae926f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-01 23:28:48 +0530

    examples

--- currency/txt1/serv1.go
+++ currency/txt1/serv1.go
@@ -19,8 +19,25 @@ var currencies = curr.Load("../../../data.csv")
 // Buffers are read in one shot creating opportunities for missing data
 
 func main() {
-	var addr string
-	var network string
-	flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
-	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-	flag.Parse()
+var addr string
+var network string
+flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+flag.Parse()
+
+// validate supported network protocols
+switch network {
+case "tcp", "tcp4", "tcp6", "unix":
+default:
+	log.Fatalln("unsupported network protocol:", network)
+}
+
+// create listener for provided network and host address
+ln, err := net.Listen(network, addr)
+if err != nil {
+   log.Fatal("failed to create listener:", err)
+}
+defer ln.Close()
+log.Println("***** Global Currency Service *****")
+log.Printf("Service started: (%s) %s\n", network, addr)
+
