714e0b44b31d2b7a5a59ba8db976ebc43b3f0fd5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-01 23:38:33 +0530

    examples

--- currency/txt1/serv1.go
+++ currency/txt1/serv1.go
@@ -66,3 +66,19 @@ if _, err := conn.Write([]byte("Connected...\nUsage: GET <currency, country, or 
    log.Println("error writing:", err)
    return
 }   
+
+for {
+cmdLine := make([]byte, (1024 * 4))
+n, err := conn.Read(cmdLine)
+if n == 0 || err != nil {
+   log.Println("connection read error:", err)
+   return
+}
+cmd, param := parseCommand(string(cmdLine[0:n]))
+if cmd == "" {
+   if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+      log.Println("failed to write:", err)
+      return
+   }
+   continue
+}
