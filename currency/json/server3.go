4fbbed12d523e0c5cc04ff6c16af86c92d8d45ab
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-02 20:23:00 +0530

    examples

---
+++ currency/json/server3.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+
+	curr "currency/lib"
+)
+
+var (
+	currencies = curr.Load("../../../data.csv")
+)
+
+// This program implements a simple currency lookup service over 
+// TCP or UDS. The request data is unmarshalled to Go type 
+// curr.CurrencyRequest using the encoding/json package.
+
+// The request is then used to search the list of currencies. The search 
+// result, a []curr.Currency, is marshalled as JSON array of objects and
+// sent to the client.
+
+// Focus:
+// This version of the program highlights the use of encoding packages
+// to serialize data to/from Go data types to another representation 
+// such as JSON. The program uses the bufio package to stream data to 
+// and from the client. This time however, char '}' is used as demarcation
+// instead of '\n'
+
+func main() {
+// setup flags
+var addr string
+var network string
+flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+flag.Parse()
+
+// validate supported network protocols
+switch network {
+case "tcp", "tcp4", "tcp6", "unix":
+default:
+	fmt.Println("unsupported network protocol")
+	os.Exit(1)
+}
+
