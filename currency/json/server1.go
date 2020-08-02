bcdca2245f494b779e4484a1e2abb3506a769aac
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-02 16:06:36 +0530

    examples

---
+++ currency/json/server1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	curr "curlib/lib"
+)
+
+var (
+	currencies = curr.Load("../../../data.csv")
+)
+
+// This program implements a simple currency lookup service over
+// TCP or UDS. It servers data using JSON encoded data
+// Clients send search requests as JSON objects like {"GET":"USD"}
+// The request is unmarshalled to Go type curr.CurrencyRequest{Get: "USD"}
+// The request is searched and the result a curr.Currency is marshalled
+// to JSON array of object and sent to client.
+// IO Streaming:
+// This version of server highlights the use of IO streaming when
+// using net.Conn to stream data to and from clients.
+
+func main() {
+	// setup flags
+	var addr string
+	var network string
+	flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.Parse()
+
+	// validate supported network protocols
+	switch network {
+	case "tcp", "tcp4", "tcp6", "unix":
+	default:
+		fmt.Println("unsupported network protocol")
+		os.Exit(1)
+	}
+
