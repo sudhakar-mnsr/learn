f23cd459a3a1085d15f446bbcc1bb61305c4c423
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-02 21:15:24 +0530

    examples

--- currency/json/server5.go
+++ currency/json/server5.go
@@ -22,17 +22,38 @@ var (
 // handle them.
 
 func main() {
-// setup flags
-var addr string
-var network string
-flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp", "tcp4", "tcp6", "unix":
-default:
-	fmt.Println("unsupported network protocol")
-	os.Exit(1)
+   // setup flags
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
+   default:
+   	fmt.Println("unsupported network protocol")
+   	os.Exit(1)
+   }
+   
+   ln, err := net.Listen(network, addr)
+   if err != nil {
+      log.Println(err)
+      os.Exit(1)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started (%s) %s\n", network, addr)
+   
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         log.Println(err)
+         conn.Close()
+         continue
+      }
+      log.Println("connected to ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
 }
