a3f70c8474ecac1af9a04df22d4c4f53e101c098
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-03 23:44:23 +0530

    examples

--- currency/json/server7.go
+++ currency/json/server7.go
@@ -26,30 +26,57 @@ var (
 // connection hostage by taking long time to send or receive data
 
 func main() {
-// setup flags
-var addr string
-var network string
-flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp", "tcp4", "tcp6", "unix":
-default:
-	fmt.Println("unsupported network protocol")
-	os.Exit(1)
+   // setup flags
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
+   default:
+   	fmt.Println("unsupported network protocol")
+   	os.Exit(1)
+   }
+   
+   ln, err := net.Listen(network, addr)
+   if err != nil {
+      log.Println(err)
+      os.Exit(1)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started: (%s) %s\n", network, addr)
+   
+   acceptDelay := time.Millisecond * 10
+   acceptCount := 0
+    
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         switch e := err.(type) {
+         case net.Error:
+            if e.Temporary() {
+               if acceptCount > 5 {
+                  log.Printf("unable to connect after %d retries: %v", err)
+                  return
+               }
+               acceptDelay *= 2
+               acceptCount++
+               time.Sleep(acceptDelay)
+               continue
+            }
+         default:
+            log.Println(err)
+            conn.Close()
+            continue
+         }
+         acceptDelay = time.Millisecond * 10
+         acceptCount = 0
+      }
+      log.Println("connected to ", conn, RemoteAddr())
+      go handleConnection(conn)
+   }
 }
-
-ln, err := net.Listen(network, addr)
-if err != nil {
-   log.Println(err)
-   os.Exit(1)
-}
-defer ln.Close()
-log.Println("***** Global Currency Service *****")
-log.Printf("Service started: (%s) %s\n", network, addr)
-
-acceptDelay := time.Millisecond * 10
-acceptCount := 0
- 
