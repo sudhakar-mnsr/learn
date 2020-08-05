5292079bd7b6d19b2606ac07c2652b20c9557d56
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-05 13:11:34 +0530

    examples

--- currency/json/clientjson3.go
+++ currency/json/clientjson3.go
@@ -24,48 +24,49 @@ const prompt = "currency"
 // Further code also implements connection retry 
 
 func main() {
-var addr string
-var network string
-flag.StringVar(&addr, "e", "localhost:4040", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.Parse()
-
-// create a dialer to configure its settings instead of using
-// the default dialer from net.Dial() function
-dialer := &net.Dialer{
-   Timeout: time.Second * 300,
-   KeepAlive: time.Minute * 5,
-}
-
-// simple dialing strategy with retry with a simple backoff.
-// More sophisticated retry strategies follow similar pattern
-// but may include exponential backoff delay
-var (
-   conn net.Conn
-   err error
-   connTries = 0
-   connMaxRetries = 3
-   connSleepRetry = time.Second * 1
-)
-
-for connTries < connMaxRetries {
-   fmt.Println("creating connection socket to ", addr)
-   conn, err = dialer.Dial(network, addr)
-   if err != nil {
-      fmt.Println("failed to create socket:", err)
-      switch nerr := err.(type) {
-      case net.Error:
-         if nerr.Temporary() {
-            connTries++
-            fmt.Println("trying again in:", connSleepRetry)
-            time.Sleep(connSleepRetry)
-            continue
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", "localhost:4040", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.Parse()
+   
+   // create a dialer to configure its settings instead of using
+   // the default dialer from net.Dial() function
+   dialer := &net.Dialer{
+      Timeout: time.Second * 300,
+      KeepAlive: time.Minute * 5,
+   }
+   
+   // simple dialing strategy with retry with a simple backoff.
+   // More sophisticated retry strategies follow similar pattern
+   // but may include exponential backoff delay
+   var (
+      conn net.Conn
+      err error
+      connTries = 0
+      connMaxRetries = 3
+      connSleepRetry = time.Second * 1
+   )
+   
+   for connTries < connMaxRetries {
+      fmt.Println("creating connection socket to ", addr)
+      conn, err = dialer.Dial(network, addr)
+      if err != nil {
+         fmt.Println("failed to create socket:", err)
+         switch nerr := err.(type) {
+         case net.Error:
+            if nerr.Temporary() {
+               connTries++
+               fmt.Println("trying again in:", connSleepRetry)
+               time.Sleep(connSleepRetry)
+               continue
+            }
+            // non-recoverable error
+            fmt.Println("unable to recover")
+            os.Exit(1)
+         default: 
+            os.Exit(1)
          }
-         // non-recoverable error
-         fmt.Println("unable to recover")
-         os.Exit(1)
-      default: 
-         os.Exit(1)
       }
       break
    }
@@ -75,14 +76,14 @@ for connTries < connMaxRetries {
    }
    defer conn.Close()
    fmt.Println("connected to currency service:", addr)
-
+   
    var param string
-
+   
    for {
       fmt.Print(prompt, "> ")
       _, err = fmt.Scanf("%s", &param)
       if err != nil {
-         fmt.Println("Usage: <search string or *>)
+         fmt.Println("Usage: <search string or *>")
          continue
       }
       req := curr.CurrencyRequest{Get: param}
@@ -99,7 +100,7 @@ for connTries < connMaxRetries {
             continue
          }
       }
-
+   
       // Display response
       var currencies []curr1.Currency
       err = json.NewDecoder(conn).Decode(&currencies)
