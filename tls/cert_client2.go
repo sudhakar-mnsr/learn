2f93a219e5d2f7206f0b826cce0463ae1ac004e1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-07 23:57:55 +0530

    examples

--- tls/cert_client2.go
+++ tls/cert_client2.go
@@ -11,7 +11,7 @@ func main() {
    // Request /hello over port 8080 via GET method
    // r, err := http.Get("http://localhost:8080/hello")
    // Request /hello over HTTPS port 8443 via the GET method
-   r, err := http.Get("https://localhost:8443/hello")
+   // r, err := http.Get("https://localhost:8443/hello")
    // Create a CA certificate pool and add cert.pem to it
    caCert, err := ioutil.ReadFile("cert.pem"
    if err != nil {
@@ -19,6 +19,21 @@ func main() {
    }
    caCertPool := x509.NewCertPool()
    caCertPool.AppendCertsFromPEM(caCert)
+
+   // Create a HTTPS cleint and supply the created CA pool
+   client := &http.Client{
+           Transport: &http.Transport{
+           TLSClientConfig: &tls.Config{
+              RootCAs: caCertPool,
+           },
+        },
+   }
+
+   // Request /hello via the created HTTPS client over 8443 via GET
+   r, err := client.Get("https://localhost:8443/hello")
+   if err != nil {
+      log.Fatal(err)
+   }
    
    defer r.Body.Close()
    body, err := ioutil.ReadAll(r.Body)
