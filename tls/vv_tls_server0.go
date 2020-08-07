adb5071f57140906a062cb8eb54f5e8a7b84948c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-08 04:09:48 +0530

    examples

--- tls/vv_tls_server0.go
+++ tls/vv_tls_server0.go
@@ -22,71 +22,75 @@ var (
 // Tls certificates at server 
 
 func main() {
-var addr, network, cert, key string
-flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.StringVar(&cert, "cert", "/tmp/certs/cert.pem", "public cert")
-flag.StringVar(&key, "key", "/tmp/certs/key.pem", "private key")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp", "tcp4", "tcp6", "unix":
-default:
-	fmt.Println("unsupported network protocol")
-	os.Exit(1)
-}
-
-// load server cert by providing the private key that generated it.
-cert, err := tls.LoadX509KeyPair(cert, key)
-if err != nil {
-   log.Fatal(err)
-}
-
-tlsConfig := &tls.Config{
-             Certificates: []tls.Certificate{cer},
-}
-
-// instead of net.Listen, we now use tls.Listen to start a listener
-// on the secure port
-ln, err := tls.Listen(network, addr, tlsConfig)
-if err != nil {
-   log.Println(err)
-}
-defer ln.Close()
-log.Println("***** Global Currency Service *****")
-log.Printf("Service started: (%s) %s; server cert %s\n", network, addr, cert)
-
-acceptDelay := time.Millisecond *10
-acceptCount := 0
-
-for {
-   conn, err := ln.Accept()
+   var addr, network, cert, key string
+   flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.StringVar(&cert, "cert", "/tmp/certs/cert.pem", "public cert")
+   flag.StringVar(&key, "key", "/tmp/certs/key.pem", "private key")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
+   default:
+   	fmt.Println("unsupported network protocol")
+   	os.Exit(1)
+   }
+   
+   // load server cert by providing the private key that generated it.
+   cer, err := tls.LoadX509KeyPair(cert, key)
    if err != nil {
-      switch e := err.(type) {
-      case net.Error:
-         if e.Temporary() {
-            if acceptCount > 5
-               log.Fatalf("unable fo connect after %d retries, acceptCount, err)
+      log.Fatal(err)
+   }
+   
+   tlsConfig := &tls.Config{
+                Certificates: []tls.Certificate{cer},
+   }
+   
+   // instead of net.Listen, we now use tls.Listen to start a listener
+   // on the secure port
+   ln, err := tls.Listen(network, addr, tlsConfig)
+   if err != nil {
+      log.Println(err)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started: (%s) %s; server cert %s\n", network, addr, cert)
+   
+   acceptDelay := time.Millisecond *10
+   acceptCount := 0
+   
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         switch e := err.(type) {
+         case net.Error:
+            if e.Temporary() {
+               if acceptCount > 5 {
+                  log.Fatalf("unable fo connect after %d retries: %v", acceptCount, err)
+               }
                acceptDelay *= 2
                acceptCount++ 
                time.Sleep(acceptDelay)
                continue
             }
-      default:
-         log.Println(err)
-         if err != conn.Close(); err != nil {
-            log.Fatal(err)
+         default:
+            log.Println(err)
+            if err := conn.Close(); err != nil {
+               log.Fatal(err)
+            }
+            continue
          }
-         continue
+         acceptDelay = time.Millisecond * 10
+         acceptCount = 0
       }
-      acceptDelay = time.Millisecond * 10
+      log.Println("securely connected to remote client ", conn.RemoteAddr())
       go handleConnection(conn)
    }
 }  
 
 func handleConnection(conn net.Conn) {
-   defer func {
+   defer func() {
       if err := conn.Close(); err != nil {
          log.Println("error closing connection:", err)
       }
@@ -98,30 +102,31 @@ func handleConnection(conn net.Conn) {
    if err := conn.SetDeadline(time.Now().Add(time.Second *45)); err != nil {
       log.Println("failed to set deadline:", err)
       return
+   }
    // Command loop
    for {
-      dec = json.NewDecoder(conn)
+      dec := json.NewDecoder(conn)
       var req curr.CurrencyRequest
       if err := dec.Decode(&req); err != nil {
-                switch err := err.(type) {
-                case net.Error:
-                if err.Timeout() {
-                   log.Println("deadline reached, disconnecting.....")
-                }
-                log.Println("network error:", err)
-                return
-                default:
-                   if err = io.EOF {
-                      log.Println("closing connection:", err)
-                      return
-                   }
-                   enc := json.NewEncoder(conn)
-                   if encerr := enc.Encode(&curr.CurrencyError{Error: err.Error()}); encerr = nil {    
-                      log.Println("failed error encoding", err)
-                      return
-                    }
-                    continue
-         }
+           switch err := err.(type) {
+           case net.Error:
+              if err.Timeout() {
+                 log.Println("deadline reached, disconnecting.....")
+              }
+              log.Println("network error:", err)
+              return
+           default:
+              if err == io.EOF {
+                 log.Println("closing connection:", err)
+                 return
+              }
+              enc := json.NewEncoder(conn)
+              if encerr := enc.Encode(&curr.CurrencyError{Error: err.Error()}); encerr != nil {    
+                 log.Println("failed error encoding", err)
+                 return
+              }
+              continue
+           }
       }
       
       // search currencies
@@ -131,7 +136,7 @@ func handleConnection(conn net.Conn) {
       if err := enc.Encode(&result); err != nil {
          switch err := err.(type) {
          case net.Error:
-            log.Println("failed to send response:"err
+            log.Println("failed to send response:", err)
             return
          default:
             if encerr := enc.Encode(&curr.CurrencyError{Error: err.Error()}); encerr != nil {
@@ -143,7 +148,7 @@ func handleConnection(conn net.Conn) {
       }
       
       if err := conn.SetDeadline(time.Now().Add(time.Second * 90)); err != nil {
-         log.Println(failed to set deadline:", err)
+         log.Println("failed to set deadline:", err)
          return
       } 
    }
