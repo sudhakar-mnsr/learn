317a5a690f16d7f16704b0d06299ae57efcdff12
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-08 02:52:29 +0530

    examples

---
+++ tls/vv_tls_server0.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	curr "currency/lib"
+)
+
+var (
+	currencies = curr.Load("../../data.csv")
+)
+
+// Focus:
+// Tls certificates at server 
+
+func main() {
+var addr, network, cert, key string
+flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
+flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+flag.StringVar(&cert, "cert", "../certs/localhost-cert.pem", "public cert")
+flag.StringVar(&key, "key", "../certs/localhost-key.pem", "private key")
+flag.Parse()
+
+// validate supported network protocols
+switch network {
+case "tcp", "tcp4", "tcp6", "unix":
+default:
+	fmt.Println("unsupported network protocol")
+	os.Exit(1)
+}
