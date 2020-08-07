9900d49835b19a08acea2ce920edfee7dadad02f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-07 23:46:29 +0530

    examples

---
+++ tls/cert_server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+   "io"
+   "log"
+   "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+   io.WriteString(w, "Hello, world!\n")
+}
+
+func main() {
+  http.HandleFunc("/hello", helloHandler)
+  // Listen to port 8080 and wait 
+  // log.Fatal(http.ListenAndServe(":8080", nil))
+  // Listen to HTTPS connections on port 8443 and wait
+  log.Fatal(http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)) 
+}
