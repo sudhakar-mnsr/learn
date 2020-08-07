642f14b76327593b9396b788320f25915cc83d55
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-08 02:58:23 +0530

    examples

--- tls/vv_tls_server0.go
+++ tls/vv_tls_server0.go
@@ -36,3 +36,23 @@ default:
 	fmt.Println("unsupported network protocol")
 	os.Exit(1)
 }
+
+// load server cert by providing the private key that generated it.
+cert, err := tls.LoadX509KeyPair(cert, key)
+if err != nil {
+   log.Fatal(err)
+}
+
+tlsConfig := &tls.Config{
+             Certificates: []tls.Certificate{cer},
+}
+
+// instead of net.Listen, we now use tls.Listen to start a listener
+// on the secure port
+ln, err := tls.Listen(network, addr, tlsConfig)
+if err != nil {
+   log.Println(err)
+}
+defer ln.Close()
+log.Println("***** Global Currency Service *****")
+log.Printf("Service started: (%s) %s; server cert %s\n", network, addr, cert)
