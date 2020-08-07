f34401ae905812f5b6c0c571b0b7993074bc5387
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-08-07 23:40:47 +0530

    examples

---
+++ tls/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+"fmt"
+"io/ioutil"
+"log"
+"net/http"
+)
+
+func main() {
+   r, err := http.Get("http://localhost:8080/hello")
+   if err != nil {
+      log.Fatal(err)
+   }
+   
+   defer r.Body.Close()
+   body, err := ioutil.ReadAll(r.Body)
+   
+   if err != nil {
+      log.Fatal(err)
+   }
+   
+   fmt.Printf("%s\n", body)
+}
